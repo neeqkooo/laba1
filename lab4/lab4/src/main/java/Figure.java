3f86867939573739ae64793e3029941a43767bf3
neeqkooo <126478176+neeqkooo@users.noreply.github.com>
2023-04-17 22:34:51 +0200

    Delete lab4/lab4 directory

--- lab4/lab4/src/main/java/Figure.java
+++
@@ -1,5 +0,0 @@
-public abstract class Figure {
-    // Общие свойства и методы для всех геометрических фигур
-    public abstract double getArea(); // Метод для вычисления площади фигуры
-    public abstract double getPerimeter(); // Метод для вычисления периметра фигуры
-}
