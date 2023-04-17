e36f0293be3a71c66bc004f512698093d6be027f
neeqkooo <126478176+neeqkooo@users.noreply.github.com>
2023-04-17 22:41:52 +0200

    Delete FormProcessor.java

--- lab6/lab6/src/main/java/FormProcessor.java
+++
@@ -1,9 +0,0 @@
-public class FormProcessor {
-    public String processForm(String name, String email) {
-
-        String result = "Name: " + name + "<br>";
-        result += "Email: " + email + "<br>";
-
-        return result;
-    }
-}
